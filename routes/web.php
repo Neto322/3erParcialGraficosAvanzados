1efc1b412209c0c71d2ff4f6802457321b254e90
sofiabelmar <42181326+sofiabelmar@users.noreply.github.com>
2021-04-16 18:48:55 -0700

    organizacion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TableroController;
+use App\Http\Controllers\Admin\TableroController;
 
 /*
 |--------------------------------------------------------------------------
